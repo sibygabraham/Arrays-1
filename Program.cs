b5947ed73fe99136fd952ca764bfea209cc43acd
hello <hello@LAPTOP-NA2F1EVT>
2021-02-05 11:22:47 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[26];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("Element Value =" + i);
+
+
+
+            }
+        }
+    }
+}
